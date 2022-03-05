e09d1dab7e8947a7b4889d5678186a961c2a1328
Borstelchen <sebastian@paetzold-family.de>
2022-03-05 13:09:48 +0100

    Initial Commit

---
+++ AndroidProducts.mk
@@ -0,0 +1,7 @@
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_zeroltexx.mk 
+
+COMMON_LUNCH_CHOICES := \
+    lineage_zeroltexx-user \
+    lineage_zeroltexx-userdebug \
+    lineage_zeroltexx-eng
